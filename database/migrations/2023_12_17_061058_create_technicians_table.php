dfdca36db0de0ab1fdcf0d06bdc50c5aac31ea48
Rajiv-Bhandari <bhandas.3282@gmail.com>
2024-01-19 10:00:11 +0545

    updated technicians table

--- database/migrations/2023_12_17_061058_create_technicians_table.php
+++ database/migrations/2023_12_17_061058_create_technicians_table.php
@@ -15,20 +15,27 @@ return new class extends Migration
             $table->unsignedBigInteger('contactnumber');
             $table->string('address');
             $table->string('email')->unique();
-            $table->string('skill');
+            $table->unsignedBigInteger('skill_id'); 
             $table->unsignedInteger('yearsofexperience')->nullable();
             $table->date('dob');
             $table->integer('totaljobs')->default(0);
             $table->string('profilepic')->nullable();
             $table->unsignedBigInteger('user_id')->nullable();
             $table->foreign('user_id')->references('id')->on('users')->onDelete('set null');
+            $table->foreign('skill_id')->references('id')->on('categories'); // Foreign key reference to categories table
             $table->integer('status')->default(TechnicianStatus::Pending);
+            $table->text('rejectmessage')->nullable();
             $table->timestamps();
         });
     }
 
     public function down()
     {
+        Schema::table('technicians', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+            $table->dropForeign(['skill_id']);
+        });
+
         Schema::dropIfExists('technicians');
     }
 };
