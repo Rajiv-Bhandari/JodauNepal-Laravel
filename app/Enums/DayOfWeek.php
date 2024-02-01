d81b550f15fc3b0962006ea5b064481f7af11fb2
Rajiv-Bhandari <bhandas.3282@gmail.com>
2024-02-01 17:10:50 +0545

    technician time slot model and enum for day

---
+++ app/Enums/DayOfWeek.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Enums;
+
+use BenSampo\Enum\Enum;
+
+/**
+ * @method static static Sunday()
+ */
+final class DayOfWeek extends Enum
+{
+    const Sunday = 1;
+    const Monday = 2;
+    const Tuesday = 3;
+    const Wednesday = 4;
+    const Thursday = 5;
+    const Friday = 6;
+    const Saturday = 7;
+}
