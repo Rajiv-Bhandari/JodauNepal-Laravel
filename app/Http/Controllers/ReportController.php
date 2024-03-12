3f1c32754340393f9755d5f34dc44502b864bd44
Rajiv-Bhandari <bhandas.3282@gmail.com>
2024-03-12 08:04:17 +0545

    display dynamic data in status & technician of report

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Enums\BookingStatus;
+use App\Models\Technician;
+use App\Models\User;
 
 class ReportController extends Controller
 {
     public function filterbooking()
     {
-        return view('report.filter-booking');
+        $technicians = User::where('usertype', 1)->get();
+
+        return view('report.filter-booking', compact('technicians'));
     }
 }
--- resources/views/report/filter-booking.blade.php
+++ resources/views/report/filter-booking.blade.php
@@ -23,41 +23,43 @@
                                 <div class="card-header text-bold">Bookings Report :</div>
                                 <div class="col-12 py-1" style="clear:both;">&nbsp;</div>
                                     <form type="GET" action="#">
-                                        <input type="hidden" value="false" name="isPdf">
-                                     
+                                        <input type="hidden" value="false" name="isPdf">                                    
 
-                                        {{-- Assets --}}
+                                        <!-- Booking Status -->
                                         <div class="col-md-6">
                                             <div class="form-group">
                                                 <label class="form-title">Booking Status</label>
-                                                <select class="form-control" id="assetSelect" name="report" value="report">
+                                                <select class="form-control" name="booking_status">
                                                     <option value="">Select Status</option>
-                                               
+                                                    @foreach(App\Enums\BookingStatus::getKeys() as $status)
+                                                        <option value="{{ $status }}">{{ ucwords($status) }}</option>
+                                                    @endforeach
                                                 </select>
-                                                @if ($errors->has('asset'))
-                                                <x-validation-errors><div class="form-group col-3 py-2">
-                                                        {{ $errors->first('asset') }}
+                                                @if ($errors->has('booking_status'))
+                                                    <x-validation-errors>
+                                                        {{ $errors->first('booking_status') }}
                                                     </x-validation-errors>
                                                 @endif
                                             </div>
                                         </div>
-                                        {{--  --}}
 
-                                        {{-- status --}}
+                                        <!-- Technician -->
                                         <div class="col-md-6">
                                             <div class="form-group">
                                                 <label class="form-title">Technician</label>
-                                                <select class="form-control px-2" name="status" >
+                                                <select class="form-control" name="technician">
                                                     <option value="">Select Technician</option>
+                                                    @foreach($technicians as $technician)
+                                                        <option value="{{ $technician->id }}">{{ $technician->name }}</option>
+                                                    @endforeach
                                                 </select>
-                                                @if ($errors->has('status'))
-                                                <x-validation-errors><div class="form-group col-3 py-2">
-                                                        {{ $errors->first('status') }}
+                                                @if ($errors->has('technician'))
+                                                    <x-validation-errors>
+                                                        {{ $errors->first('technician') }}
                                                     </x-validation-errors>
                                                 @endif
                                             </div>
                                         </div>
-                                        {{--  --}}
                                         
                                         <div class="row p-2">
                                             <div class="col-2 px-2" style="float: inline-block">
