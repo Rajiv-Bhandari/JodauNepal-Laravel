c63c1b956f8c33481c356392c0b3aeb077cbb69a
Rajiv-Bhandari <bhandas.3282@gmail.com>
2024-03-24 10:07:24 +0545

    removed pushmenu and dark/light mode in technician layout

--- resources/views/technicianlayout/navbar.blade.php
+++ resources/views/technicianlayout/navbar.blade.php
@@ -3,9 +3,9 @@
   <nav class="main-header navbar navbar-expand navbar-white navbar-light">
     <!-- Left navbar links -->
     <ul class="navbar-nav">
-      <li class="nav-item">
+      <!-- <li class="nav-item">
         <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-      </li>
+      </li> -->
       <li class="nav-item d-none d-sm-inline-block">
         <a class="nav-link">
             Hello, {{ auth()->user()->name }}
@@ -51,26 +51,6 @@
           <i class="fas fa-expand-arrows-alt"></i>
         </a>
       </li>
-      <li class="nav-item">
-        <a id="dark-mode-toggle" class="nav-link" onclick="toggleDarkMode()">
-          <i class="fas fa-moon"></i>
-        </a>
-      </li>
     </ul>
   </nav>
   <!-- /.navbar -->
-
-<script>
-    document.getElementById('notificationsDropdown').addEventListener('click', function () {
-        document.querySelector('.dropdown-menu').classList.toggle('show');
-    });
-
-    // Close dropdown when clicking outside
-    document.addEventListener('click', function (event) {
-        var dropdown = document.querySelector('.dropdown-menu');
-        var target = event.target;
-        if (!target.closest('.nav-item.dropdown') && !target.classList.contains('dropdown-menu')) {
-            dropdown.classList.remove('show');
-        }
-    });
-</script>
