14c64f78d57903a600585f6c677b82ecd41f152f
Rajiv-Bhandari <bhandas.3282@gmail.com>
2024-04-03 08:36:52 +0545

    working in profile of technician pannel

--- resources/views/technicianlayout/sidebar.blade.php
+++ resources/views/technicianlayout/sidebar.blade.php
@@ -44,6 +44,14 @@
               </p>
             </a>
           </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link" >
+              <i class="nav-icon fas fa-user"></i>
+              <p>
+                Profile
+              </p>
+            </a>
+          </li>
         </ul>
         <!-- Logout Button -->
         <div class="text-center mt-auto p-3">
